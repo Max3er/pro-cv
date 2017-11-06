2b5950326d00c62f7176ed915809db8ed66a4fcd
Max3er <joaott02@gmail.com>
2017-11-06 21:39:53 +0100

    alot of sass- and CSS fixes

--- js/scrip.js
+++ js/scrip.js
@@ -1,12 +1,20 @@
-$("#expand").click(function(){
 
+// Shift + and -
+
+$("#expand").click(function(){
     var ct = $(this).text();
     if (ct == "+") {
-
-            $(this).text("-");
-            
+        $(this).text("-");
     } else {
-
-            $(this).text("+");
+        $(this).text("+");
     };
+});
+
+// Expand div on click
+
+$("#expand").click(function () {
+        $expand = $(this);
+            $content = $(".job-description");
+            $content.slideToggle(500, function () {
+        });
 });
