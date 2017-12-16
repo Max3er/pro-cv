74d7a2d87a50c8f8fbc3bc14ed6c63b1a8f770e4
Max3er <joaott02@gmail.com>
2017-12-16 22:21:17 +0100

    framework standard for the mobile is done

--- js/scrip.js
+++ js/scrip.js
@@ -1,29 +1,31 @@
-
 // Shift + and -
-
-$("#expand").click(function(){
+$(".expand").click(function(){
+    $(this).removeClass('is-active');
     var ct = $(this).text();
     if (ct == "+") {
         $(this).text("-");
+        $(this).addClass('is-active');
     } else {
         $(this).text("+");
+        $(this).removeClass('is-active');
     };
-    
 });
 
 // Expand div on click - work experience
-
-$("#expand").click(function () {
-   // $expand = $(this);
-   // $content = $(".job-description");
-   // $content.slideToggle(500, function () {
-    $(this).closest(".job-description").slideToggle(500, function () {
-    });
+$(".expand").click(function () {
+    const myDescription = document.getElementsByClassName("job-description");
+        $(this).next('div').next().slideToggle(500, function () {
+        });
 });
 
 // portfolio menu
-
 $('ul li a').click(function() {
     $('ul li').removeClass('is-active');
     $(this).closest('li').addClass('is-active');
+});
+
+//portfolio menu
+$(".port-item").click(function(){
+    $(".port-item").removeClass('is-active');
+    $(this).addClass('is-active');
 });
